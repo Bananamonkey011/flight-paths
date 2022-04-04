48e2e2763648f05fa52e447de32e09e126ea0e65
Manan Jain <manan@wirelessprv-10-194-163-75.near.illinois.edu>
2022-04-04 11:34:37 -0500

    Node class members created

--- Node.cpp
+++ Node.cpp
@@ -1,1 +1,29 @@
 #include "Node.h"
+
+Node::Node() {
+    id_ = 0;
+    name_ = "";
+    city_ = "";
+    country_ = "";
+    IATA_ = "";
+    ICAO_ = "";
+    lat_ = -999;
+    long_ = -999;
+    alt_ = -999;
+}
+
+Node::Node(int id, string name, string city, string country, string IATA, string ICAO, double lat, double longi, int alt){
+    id_ = id;
+    name_ = name;
+    city_ = city;
+    country_ = country;
+    IATA_ = IATA;
+    ICAO_ = ICAO;
+    lat_ = lat;
+    long_ = longi;
+    alt_ = alt;
+}
+
+void Node::addAirport(Node* a, double d){
+    airports_[a] = d;
+}
--- Node.h
+++ Node.h
@@ -1,9 +1,28 @@
 #pragma once
 
-#include <vector>
+#include <map>
+#include <string>
 
 using namespace std;
 
 class Node {
-    vector<Node*> neighbors;
+    //data variables
+    int id_;
+    string name_;
+    string city_;
+    string country_;
+    string IATA_;
+    string ICAO_;
+    double lat_;
+    double long_;
+    int alt_;
+    //idt we need more than these
+
+    //map of connecting airports and the route distance
+    map<Node*, double> airports_;
+
+    Node();
+    Node(int id, string name, string city, string country, string IATA, string ICAO, double lat, double longi, int alt);
+
+    void addAirport(Node* a, double d);
 };
